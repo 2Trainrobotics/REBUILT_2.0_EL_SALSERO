f4db3e8868b27f4138e018cbaeed7d7281744857
Wilmer <395robotics@gmail.com>
2026-03-25 22:18:53 -0400

    Today, we configured the Configs.java, ModuleConstants.java, and MaxSwerve.java.

--- src/main/java/frc/robot/constants/ModuleConstants.java
+++ src/main/java/frc/robot/constants/ModuleConstants.java
@@ -22,15 +22,15 @@ public static final int kDrivingMotorPinionTeeth = 14;
 
 // Calculations required for drivign motor conversion fctors and feed forward
  public static final double kDrivingMotorFreeSpeedRps = NeoMotorConstants.kFreeSpeedRpm / 60;
- public static final double kWheelDiametersMeters = 0.0762; 
- public static final double kWheelCircumferenceMeters = kWheelDiametersMeters * Math.PI; 
+ public static final double kWheelDiameterMeters = 0.0762; 
+ public static final double kWheelCircumferenceMeters = kWheelDiameterMeters * Math.PI; 
 
  /* 45 teeth on the wheel's bevel gear, 22 teeth on the first-stage spur gear, 
  15 teeth on the bevel pinion. 
  */
 
  public static final double kDrivingMotorReduction = (45.0 * 22) / (kDrivingMotorPinionTeeth * 15); 
- public static final double kDrivingWheelFreeSpeedRps = (kDrivingMotorFreeSpeedRps * kWheelCircumferenceMeters)
+ public static final double kDriveWheelFreeSpeedRps = (kDrivingMotorFreeSpeedRps * kWheelCircumferenceMeters)
     / kDrivingMotorReduction;
 
     // This class is declared inside ModuleConstants.java since 
--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -1,3 +1,25 @@
 package frc.robot.subsystems;
 
 
+import com.ctre.phoenix6.hardware.Pigeon2;
+
+import edu.wpi.first.hal.FRCNetComm.tInstances;
+import edu.wpi.first.hal.FRCNetComm.tResourceType;
+import edu.wpi.first.hal.HAL;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import frc.robot.constants.DriveConstants;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard; 
+
+public class DriveSubsystem extends SubsystemBase {
+    // We create the MaxSwerveModules
+    
+
+
+    }
