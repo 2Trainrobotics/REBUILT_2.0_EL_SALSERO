99a8ee5aff9878015c07f1529b8658ec1b01a6b8
2 Train Robotics <157223793+2Trainrobotics@users.noreply.github.com>
2026-03-27 13:47:13 -0400

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,9 +1,18 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot;
 
 import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkBase.SoftLimitDirection;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-import com.revrobotics.CANSparkBase.SoftLimitDirection;
+
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkBase.ControlType;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
@@ -12,44 +21,51 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import frc.robot.constants.ShooterConstants;
-import frc.robot.subsystems.ShooterSubsystem;
 
 public class Robot extends TimedRobot {
 
-    // Drive motors
-    private final CANSparkMax driveLeftLeader  = new CANSparkMax(1, MotorType.kBrushed);
-    private final CANSparkMax driveLeftFollower = new CANSparkMax(2, MotorType.kBrushed);
-    private final CANSparkMax driveRightLeader = new CANSparkMax(3, MotorType.kBrushed);
-    private final CANSparkMax driveRightFollower = new CANSparkMax(4, MotorType.kBrushed);
+    // Drive Motors (still CANSparkMax)
+    private CANSparkMax driveLeftLeader = new CANSparkMax(1, MotorType.kBrushed);
+    private CANSparkMax driveLeftFollower = new CANSparkMax(2, MotorType.kBrushed);
+    private CANSparkMax driveRightLeader = new CANSparkMax(3, MotorType.kBrushed);
+    private CANSparkMax driveRightFollower = new CANSparkMax(4, MotorType.kBrushed);
 
-    // Climber motors
-    private final CANSparkMax climberLeader = new CANSparkMax(5, MotorType.kBrushless);
-    private final CANSparkMax climberFollower = new CANSparkMax(6, MotorType.kBrushless);
+    // Climber
+    private CANSparkMax climberLeader = new CANSparkMax(5, MotorType.kBrushless);
+    private CANSparkMax climberFollower = new CANSparkMax(6, MotorType.kBrushless);
 
-    // Intake motors
-    private final CANSparkMax intakeRoller = new CANSparkMax(7, MotorType.kBrushless);
-    private final CANSparkMax intakeSwivel = new CANSparkMax(8, MotorType.kBrushless);
+    // Intake
+    private CANSparkMax intakeRoller = new CANSparkMax(7, MotorType.kBrushless);
+    private CANSparkMax intakeSwivel = new CANSparkMax(8, MotorType.kBrushless);
 
-    // Subsystems
-    private final ShooterSubsystem shooterSubsystem = new ShooterSubsystem();
+    // Shooter (Spark Flex)
+    private SparkFlex shooterLeft = new SparkFlex(ShooterConstants.kLeftShooterCanId);
+    private SparkFlex shooterRight = new SparkFlex(ShooterConstants.kRightShooterCanId);
+
+    private RelativeEncoder shooterLeftEncoder;
+    private RelativeEncoder shooterRightEncoder;
+
+    private SparkClosedLoopController shooterLeftController;
+    private SparkClosedLoopController shooterRightController;
 
     // Controllers
-    private final XboxController driverController = new XboxController(0);
-    private final XboxController operatorController = new XboxController(1);
+    private XboxController driverController = new XboxController(0);
+    private XboxController operatorController = new XboxController(1);
 
-    // Autonomous chooser
-    private final SendableChooser<String> autoChooser = new SendableChooser<>();
-    private String autoSelected;
-    private final Timer autoTimer = new Timer();
+    private static final String kDefaultAuto = "Default";
+    private static final String kCustomAuto = "My Auto";
+    private String m_autoSelected;
+    private static Timer m_timer = new Timer();
+    private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
     @Override
     public void robotInit() {
-        // Autonomous chooser
-        autoChooser.setDefaultOption("Default Auto", "Default");
-        autoChooser.addOption("Custom Auto", "Custom");
-        SmartDashboard.putData("Auto Choices", autoChooser);
+        // Auto chooser
+        m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
+        m_chooser.addOption("My Auto", kCustomAuto);
+        SmartDashboard.putData("Auto choices", m_chooser);
 
-        // Drive setup
+        // Drive motors configuration
         driveLeftLeader.setInverted(true);
         driveLeftLeader.getEncoder().setPosition(0);
         driveLeftLeader.setIdleMode(IdleMode.kBrake);
@@ -66,7 +82,7 @@ public class Robot extends TimedRobot {
         driveRightFollower.setIdleMode(IdleMode.kBrake);
         driveRightFollower.follow(driveRightLeader);
 
-        // Climber setup
+        // Climber configuration
         climberLeader.getEncoder().setPosition(0);
         climberLeader.setIdleMode(IdleMode.kBrake);
         climberFollower.setIdleMode(IdleMode.kBrake);
@@ -75,22 +91,40 @@ public class Robot extends TimedRobot {
         climberLeader.setSoftLimit(SoftLimitDirection.kReverse, 0);
         climberLeader.enableSoftLimit(SoftLimitDirection.kForward, true);
         climberLeader.enableSoftLimit(SoftLimitDirection.kReverse, true);
+
+        // Shooter setup
+        shooterLeftEncoder = shooterLeft.getEncoder();
+        shooterRightEncoder = shooterRight.getEncoder();
+
+        shooterLeftController = shooterLeft.getClosedLoopController();
+        shooterRightController = shooterRight.getClosedLoopController();
+
+        shooterLeftController.setPID(ShooterConstants.kP, ShooterConstants.kI, ShooterConstants.kD);
+        shooterRightController.setPID(ShooterConstants.kP, ShooterConstants.kI, ShooterConstants.kD);
+
+        shooterLeftController.setFF(ShooterConstants.kFF);
+        shooterRightController.setFF(ShooterConstants.kFF);
+
+        shooterLeftEncoder.setPosition(0);
+        shooterRightEncoder.setPosition(0);
     }
 
     @Override
+    public void robotPeriodic() {}
+
+    @Override
     public void autonomousInit() {
-        autoSelected = autoChooser.getSelected();
-        System.out.println("Auto selected: " + autoSelected);
-        autoTimer.reset();
-        autoTimer.start();
+        m_autoSelected = m_chooser.getSelected();
+        System.out.println("Auto selected: " + m_autoSelected);
+        m_timer.reset();
+        m_timer.start();
     }
 
     @Override
     public void autonomousPeriodic() {
-        // Simple auto: drive forward 2 seconds
-        if (!autoTimer.hasElapsed(2)) {
-            driveLeftLeader.set(0.5);
-            driveRightLeader.set(0.5);
+        if (!m_timer.hasElapsed(2)) {
+            driveLeftLeader.set(1);
+            driveRightLeader.set(1);
         } else {
             driveLeftLeader.set(0);
             driveRightLeader.set(0);
@@ -98,55 +132,76 @@ public class Robot extends TimedRobot {
     }
 
     @Override
+    public void teleopInit() {}
+
+    @Override
     public void teleopPeriodic() {
-        driveControl();
-        intakeControl();
-        shooterControl();
-        climberControl();
-    }
 
-    private void driveControl() {
-        double leftPower = applyDeadband(driverController.getLeftY());
-        double rightPower = applyDeadband(driverController.getRightY());
+        /////////////////
+        // DRIVETRAIN
+        /////////////////
+        double driveLeftPower = driverController.getLeftY();
+        double driveRightPower = driverController.getRightY();
 
-        driveLeftLeader.set(leftPower);
-        driveRightLeader.set(rightPower);
+        if (Math.abs(driveLeftPower) < 0.1) driveLeftPower = 0;
+        if (Math.abs(driveRightPower) < 0.1) driveRightPower = 0;
 
-        SmartDashboard.putNumber("Drive Left Power", leftPower);
-        SmartDashboard.putNumber("Drive Right Power", rightPower);
-    }
+        SmartDashboard.putNumber("driveLeftPower", driveLeftPower);
+        SmartDashboard.putNumber("driveRightPower", driveRightPower);
 
-    private void intakeControl() {
-        double rollerPower = applyDeadband(operatorController.getLeftY());
-        double swivelPower = applyDeadband(operatorController.getRightY());
+        driveLeftLeader.set(driveLeftPower);
+        driveRightLeader.set(driveRightPower);
 
-        intakeRoller.set(rollerPower);
-        intakeSwivel.set(swivelPower);
-    }
+        /////////////////
+        // CLIMBER
+        /////////////////
+        // Example: uncomment and use buttons if needed
+        // if (driverController.getLeftBumper()) climberLeader.set(-0.5);
+        // if (driverController.getRightBumper()) climberLeader.set(0.5);
 
-    private void shooterControl() {
+        /////////////////
+        // INTAKE
+        /////////////////
+        double intakeRollerPower = operatorController.getLeftY();
+        double intakeSwivelPower = operatorController.getRightY();
+
+        if (Math.abs(intakeRollerPower) < 0.1) intakeRollerPower = 0;
+        if (Math.abs(intakeSwivelPower) < 0.1) intakeSwivelPower = 0;
+
+        intakeRoller.set(intakeRollerPower);
+        intakeSwivel.set(intakeSwivelPower);
+
+        /////////////////
+        // SHOOTER
+        /////////////////
         boolean enableShooter = operatorController.getLeftBumper() || operatorController.getRightBumper();
         if (enableShooter) {
-            shooterSubsystem.setShooter(1.0);
+            shooterLeftController.setSetpoint(ShooterConstants.kDefaultShooterRPM, ControlType.kVelocity);
+            shooterRightController.setSetpoint(ShooterConstants.kDefaultShooterRPM, ControlType.kVelocity);
         } else {
-            shooterSubsystem.stop();
+            shooterLeft.set(0);
+            shooterRight.set(0);
         }
+
+        SmartDashboard.putNumber("shooterLeftRPM", shooterLeftEncoder.getVelocity());
+        SmartDashboard.putNumber("shooterRightRPM", shooterRightEncoder.getVelocity());
     }
 
-    private void climberControl() {
-        boolean climbUp = driverController.getRightBumper();
-        boolean climbDown = driverController.getLeftBumper();
+    @Override
+    public void disabledInit() {}
 
-        if (climbUp) {
-            climberLeader.set(0.5);
-        } else if (climbDown) {
-            climberLeader.set(-0.5);
-        } else {
-            climberLeader.set(0);
-        }
-    }
+    @Override
+    public void disabledPeriodic() {}
 
-    private double applyDeadband(double value) {
-        return Math.abs(value) < 0.1 ? 0 : value;
-    }
+    @Override
+    public void testInit() {}
+
+    @Override
+    public void testPeriodic() {}
+
+    @Override
+    public void simulationInit() {}
+
+    @Override
+    public void simulationPeriodic() {}
 }
