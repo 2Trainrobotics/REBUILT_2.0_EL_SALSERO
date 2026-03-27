0acd8bf1a988d6a1f1a4eee7d4825970e308aea1
2 Train Robotics <157223793+2Trainrobotics@users.noreply.github.com>
2026-03-27 13:40:45 -0400

    Refactor ShooterSubsystem to use CANSparkMax

--- src/main/java/frc/robot/constants/ShooterConstants.java
+++ src/main/java/frc/robot/constants/ShooterConstants.java
@@ -1,38 +1,41 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.spark.SparkClosedLoopController;
-import com.revrobotics.spark.SparkFlex;
-import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANPIDController;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.ShooterConstants;
 
 public class ShooterSubsystem extends SubsystemBase {
 
-    private final SparkFlex m_leftShooter = new SparkFlex(ShooterConstants.kLeftShooterCanId);
-    private final SparkFlex m_rightShooter = new SparkFlex(ShooterConstants.kRightShooterCanId);
+    private final CANSparkMax m_leftShooter = new CANSparkMax(ShooterConstants.kLeftShooterCanId, MotorType.kBrushless);
+    private final CANSparkMax m_rightShooter = new CANSparkMax(ShooterConstants.kRightShooterCanId, MotorType.kBrushless);
 
     private final RelativeEncoder m_leftEncoder;
     private final RelativeEncoder m_rightEncoder;
 
-    private final SparkClosedLoopController m_leftController;
-    private final SparkClosedLoopController m_rightController;
+    private final CANPIDController m_leftController;
+    private final CANPIDController m_rightController;
 
     public ShooterSubsystem() {
         // Get encoders and controllers
         m_leftEncoder = m_leftShooter.getEncoder();
         m_rightEncoder = m_rightShooter.getEncoder();
 
-        m_leftController = m_leftShooter.getClosedLoopController();
-        m_rightController = m_rightShooter.getClosedLoopController();
+        m_leftController = m_leftShooter.getPIDController();
+        m_rightController = m_rightShooter.getPIDController();
 
         // Configure PID
-        m_leftController.setPID(ShooterConstants.kP, ShooterConstants.kI, ShooterConstants.kD);
-        m_rightController.setPID(ShooterConstants.kP, ShooterConstants.kI, ShooterConstants.kD);
-
-        // Feedforward
+        m_leftController.setP(ShooterConstants.kP);
+        m_leftController.setI(ShooterConstants.kI);
+        m_leftController.setD(ShooterConstants.kD);
         m_leftController.setFF(ShooterConstants.kFF);
+
+        m_rightController.setP(ShooterConstants.kP);
+        m_rightController.setI(ShooterConstants.kI);
+        m_rightController.setD(ShooterConstants.kD);
         m_rightController.setFF(ShooterConstants.kFF);
 
         // Reset encoders to zero
@@ -42,8 +45,8 @@ public class ShooterSubsystem extends SubsystemBase {
 
     /** Spin both shooter motors to target RPM */
     public void setShooterRPM(double rpm) {
-        m_leftController.setSetpoint(rpm, ControlType.kVelocity);
-        m_rightController.setSetpoint(rpm, ControlType.kVelocity);
+        m_leftController.setReference(rpm, CANSparkMax.ControlType.kVelocity);
+        m_rightController.setReference(rpm, CANSparkMax.ControlType.kVelocity);
     }
 
     /** Spin shooter to default RPM */
